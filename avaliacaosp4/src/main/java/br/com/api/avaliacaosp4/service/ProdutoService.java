3a2aef8910282bf92301f1b05768735ee29bb75a
Andrey <83211338+nizzorto@users.noreply.github.com>
2021-06-13 23:58:52 -0300

    Delete avaliacaosp4 directory

--- avaliacaosp4/src/main/java/br/com/api/avaliacaosp4/service/ProdutoService.java
+++
@@ -1,5 +0,0 @@
-package br.com.api.avaliacaosp4.service;
-
-public class ProdutoService {
-
-}
--- avaliacaosp4/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO User(cpf, senha) VALUES('159.230.512-12', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq');
-											--senha: 123456
